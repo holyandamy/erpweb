7fee082ec73d382ebdec078cbce36dc6daf5ad83
houduo <l>
2017-07-24 16:37:09 +0800

    no message

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,20 @@ import Main from './views/Main.vue'
 import Collectlist from './views/finance/collectlist.vue'
 import Collectedit from './views/finance/collectedit.vue'
 import Paylist from './views/finance/pay.vue'
+//公司管理
+import Cmanagement from './views/systemset/company/menu.vue'  //公司管理菜单
+import Department from './views/systemset/company/department.vue'   //部门管理
+import Bankaccount from './views/systemset/company/bankaccount.vue' //银行账户管理
+import Companyset from './views/systemset/company/companyset.vue'  //公司信息设置
+//审批设置
+import Approval from './views/systemset/approval/approval.vue' //审批设置
+import Addapproval from './views/systemset/approval/addapproval.vue'  //添加审批
+//角色管理
 
-//import Page4 from './views/nav2/Page4.vue'
-//import Page5 from './views/nav2/Page5.vue'
-//import Page6 from './views/nav3/Page6.vue'
-//import echarts from './views/charts/echarts.vue'
+//员工管理
+//操作日志
+//图片库
+
 
 let routes = [
 //  {
@@ -41,15 +50,12 @@ let routes = [
         path: '/',
         component: Home,
         name: '财务管理',
-        children: [
-            { path: '/main', component: Main, name: '主页', hidden: true },
-            {path: '/collectlist', component: Collectlist, name: '应收账款'},
-            {path: '/collectedit', component: Collectedit, name: '收款登记',hidden:true},
-            { path: '/paylist', component: Paylist, name: '应付账款' },
-            {path: '/payedit', component: Collectedit, name: '付款登记',hidden:true},
-           
+        children:[
+        	{path: '/collectlist', component: Collectlist, name: '收款管理',},
+        	{path: '/paylist', component: Paylist, name: '付款管理' },
+        	{path: '/collectedit', component: Collectedit, name: '收款登记',hidden:true},
+            {path: '/payedit', component: Collectedit, name: '付款登记',hidden:true}
         ]
-        
     },
     {
         path: '/',
@@ -61,6 +67,26 @@ let routes = [
         path: '/',
         component: Home,
         name: '系统设置',
+        children: [
+          	{path: '/cmanagement/companyset', component: Companyset, name: '公司管理'},
+          	{path: '/cmanagement/department', component: Department, name: '部门管理',hidden:true},
+          	{path: '/cmanagement/bankaccount', component: Bankaccount, name: '银行账号设置',hidden:true},
+            {path: '/cmanagement/companyset', component: Companyset, name: '公司信息设置',hidden:true},
+            {path: '/approval', component: Approval, name: '审批配置'},
+            {path: '/approval/addapproval', component: Addapproval, name: '新增审批',hidden:true},
+//        		children:[
+//        		{path: '/cmanagement/department', component: Department, name: '部门管理'},
+//        		{path: '/cmanagement/bankaccount', component: Bankaccount, name: '银行账号设置'},
+//        		{path: '/cmanagement/companyset', component: Companyset, name: '公司信息设置'}
+//        		]
+          	
+//          {path: '/aconfiguration', component: Aconfiguration, name: '审批配置'},
+//          {path: '/user', component: User, name: '角色管理' },
+//          {path: '/staff', component: Staff, name: '员工管理'},
+//          {path: '/log', component: Log, name: '操作日志' },
+//          {path: '/picture', component: Picture, name: '图片库'}
+           
+        ]
         
     },
 ];
