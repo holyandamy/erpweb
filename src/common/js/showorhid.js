58e58f811c04e84ef6482fcc7ba798a5e6ae2871
houduo <l>
2017-09-05 09:57:28 +0800

    no message

--- src/common/js/showorhid.js
+++ src/common/js/showorhid.js
@@ -1,14 +1,32 @@
-export function showorhide(eachlist){
-			let list = document.getElementsByClassName('hasid')
-			let hasid = JSON.parse(sessionStorage.getItem('info')).auths
-			for(let i =0;i<list.length;i++){
-				for(let k =0;k<hasid.length;k++){
-					if(list[i].id == hasid[k]){
-						list[i].style.display="inline-block"
-						list[i].style.opacity="1"
-						list[i].setAttribute('style', 'display: inline-block !important');
-					
-					}
+import Cookies from 'js-cookie';
+import { tokenlogin } from './config'
+export function showorhide(eachlist) {
+	let list = document.getElementsByClassName('hasid')
+	let token = Cookies.get('token')
+	let tokensession = sessionStorage.getItem('token')
+	let para = {}
+	let hasid = []
+	if(tokensession) {
+		para = {
+			token: tokensession
+		}
+	} else {
+		para = {
+			token: token
+		}
+	}
+
+	tokenlogin(para).then((res) => {
+		hasid = res.data.obj.auths
+		for(let i = 0; i < list.length; i++) {
+			for(let k = 0; k < hasid.length; k++) {
+				if(list[i].id == hasid[k]) {
+					list[i].setAttribute('style', 'display: inline-block !important');
+					console.log(list[i])
+
 				}
 			}
+		}
+	})
+
 }
