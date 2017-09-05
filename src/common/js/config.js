343dfd650fc9207be3369ec378150e08680bf97f
daijian <daijian@163.com>
2017-09-05 10:16:25 +0800

    config.js

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -33,6 +33,13 @@ export const paysave = params => { return axios.post(`${base}/finance/pay/save`,
 
 /* 发团列表 */
 export const teamlist = params => { return axios.post(`${base}/line/team/list`, params).then(res => res); };//发团列表
+export const grouplist = params => { return axios.post(`${base}/line/team/list`, params).then(res => res); };//模板列表
+export const reserveNum = params => { return axios.post(`${base}/sys/user/match`, params).then(res => res); };//预定输入手机号或公司名
+export const groupsave = params => { return axios.post(`${base}/line/team/save`, params).then(res => res); };//新增发团计划
+export const orderSave = params => { return axios.post(`${base}/order/save`, params).then(res => res); };//发团 预定
+export const orderdet = params => { return axios.post(`${base}/line/team/order/detail`, params).then(res => res); }//发团 下单详情  TODO
+export const groupdetail = params => { return axios.post(`${base}/line/team/detail`, params).then(res => res); };//发团列表 详情
+export const groupupdate = params => { return axios.post(`${base}/line/team/update`, params).then(res => res); };//发团列表 编辑
 
 /*产品分类*/
 export const linecategorylist = params => { return axios.post(`${base}/line/category/list`, params); };//获取线路管理
