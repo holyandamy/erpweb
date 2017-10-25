0a1c67f618e17071bea0c5f805c20f04a86210da
zhuziyun <zzy@tripb2b.com>
2017-10-25 16:30:56 +0800

    外部跳转链接

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -169,3 +169,6 @@ export const opensave = params => { return axios.post(`${base}/sys/open/save`, p
 
 /* 操作日志*/
 export const loglist = params => { return axios.post(`${base}/sys/log/list`, params).then(res => res); };//获取员工详情
+
+/* 跳转的URL */
+export const redirectUrls = ['/Plan/index.html']
