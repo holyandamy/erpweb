463338db3a4f8508550ec657b77412bb901148da
zhuziyun <zzy@tripb2b.com>
2017-10-26 09:14:32 +0800

    del submodule

--- webwrapper/start.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-wget http://$NODE_ENV.config.we2tu.com/$PROJECTNAME/config.json -O /usr/src/app/config/config.json
-wget http://$NODE_ENV.config.we2tu.com/$PROJECTNAME/log.json -O /usr/src/app/config/log.json
-yarn start
